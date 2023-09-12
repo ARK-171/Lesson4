566fac355b58dade339b5272d113f22d9693bee9
ARK-171 <kurgingr@rambler.ru>
2023-09-12 12:43:02 +0300

    1

---
+++ looper/src/main/java/ru/mirea/kurginau/looper/MainActivity.java
@@ -0,0 +1,42 @@
+package ru.mirea.kurginau.looper;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import ru.mirea.kurginau.looper.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        Handler mainThreadHandler = new Handler(Looper.getMainLooper()) {
+            @Override
+            public void handleMessage(Message msg) {
+
+                Log.d(MainActivity.class.getSimpleName(), "Task execute. This is result: " + msg.getData().getString("result"));
+
+            }
+        };
+        MyLooper myLooper = new MyLooper(mainThreadHandler);
+        myLooper.start();
+        binding.textMirea.setText("Мой номер по списку No16");
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Message msg = Message.obtain();
+                Bundle bundle = new Bundle();
+                bundle.putString("KEY", "mirea");
+                msg.setData(bundle);
+                myLooper.mHandler.sendMessage(msg);
+            }
+        });
+    }
+}
